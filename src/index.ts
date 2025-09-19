cfdb4f755e54d40a3728a19ab79e5804270f1e16
Daniel Rodrigo Beltrame <danielrbeltrame@gmail.com>
2025-09-18 23:29:04 -0300

    Criação das chamadas API e lógica para recueprar os cards e inserir uma unica vez pelo integrado

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,85 @@
 import 'dotenv/config';
 
-import axios from 'axios';
 import apm from 'elastic-apm-node';
 import { KambanizeService } from './services/kambanize-service';
+import { OpenaiService } from './services/openai-service';
 
 async function getData() {
-  const KBS = new KambanizeService();
-
-  KBS.getAllCards();
+  const kanbanizeApi = new KambanizeService();
+  const openaiService = new OpenaiService();
 
   const transaction = apm.startTransaction('GET post from API', 'custom');
 
   try {
-    const response = await axios.get(process.env.HOST ?? '');
-    console.log('Resposta:', response.data);
+    const allCards = await kanbanizeApi.getAllCards();
+    const cards = allCards.data.data;
+    const card_ids: number[] = cards.map((card: any) =>
+      Number(card.card_id ?? card.id)
+    );
+
+    const loggedTimes = await kanbanizeApi.getLoggedTime(card_ids);
+    const timesData = loggedTimes.data;
+    const loggedCardIds = timesData.map((lt: any) => lt.card_id);
+
+    const cardIdsToRemove = new Set(
+      timesData
+        .filter((lt: any) => lt.comment?.toLowerCase().includes('intkbnz248'))
+        .map((lt: any) => lt.card_id)
+    );
+
+    const filteredCardIds = timesData
+      .filter((lt: any) => !cardIdsToRemove.has(lt.card_id))
+      .map((lt: any) => lt.card_id);
+
+    const notLoggedCardIds = card_ids.filter(
+      (id: number) => !loggedCardIds.includes(id)
+    );
+
+    const finalCardIds = [
+      ...new Set([...filteredCardIds, ...notLoggedCardIds]),
+    ];
+
+    const cardHoursMap = finalCardIds.map((id) => {
+      const totalTime = timesData
+        .filter((lt: any) => lt.card_id === id)
+        .reduce((sum: number, lt: any) => sum + (lt.time || 0), 0);
+
+      return {
+        card_id: id,
+        hours: totalTime || 0,
+      };
+    });
+    for (const card of cardHoursMap) {
+      const details = await kanbanizeApi.getCardDatails(card.card_id);
+      const description = details.data.description;
+
+      const horasProc = await openaiService.getHorasMotivoProcessados(
+        description
+      );
+      const match = horasProc?.match(/Horas:\s*(\d+)\s+Motivo:\s*(.*)/s);
+
+      let horas = 0;
+      let motivo = horasProc;
+
+      if (match && match[1] && match[2]) {
+        horas = parseInt(match[1], 10) * 3600;
+        motivo = match[2].trim();
+      }
+      const saldoHoras = horas - card.hours;
+      const dataAtual = new Date();
+      const hoje: string = `${dataAtual.getFullYear()}-${String(
+        dataAtual.getMonth() + 1
+      ).padStart(2, '0')}-${String(dataAtual.getDate()).padStart(2, '0')}`;
+
+      if (saldoHoras > 0) {
+        const retornoInsert = await kanbanizeApi.insereLoggedTime({
+          cardId: card.card_id,
+          date: hoje,
+          time: saldoHoras,
+          comment: motivo + ' IntKbnz248',
+        });
+      }
+    }
 
     transaction.result = 'success';
 
